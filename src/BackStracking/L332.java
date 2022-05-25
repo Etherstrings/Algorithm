345eb4e1fc8fae643053cf82c2a485c03cae2f55
Ether <wuboyu5659@outlook.com>
2022-05-25 20:40:03 +0800

    05.25

---
+++ src/BackStracking/L332.java
@@ -0,0 +1,34 @@
+package BackStracking;
+
+import java.util.List;
+
+/**
+ * @author Etherstrings
+ * @create 2022-05-25 20:37 BackStracking - the name of the target package where the new class or interface will be created.  Algorithm - the name of the current project.  null.java - the name of the PHP file that will be created.  L332 - the name of the new file which you specify in the New File dialog box during the file creation.  ps - the login name of the current user.  2022/5/25 - the current system date.  20:37 - the current system time.  2022 - the current year.  05 - the current month.  25 - the current day of the month.  20 - the current hour.  37 - the current minute.  IntelliJ IDEA - the name of the IDE in which the file will be created.  5月 - the first 3 letters of the month name. Example: Jan, Feb, etc.  五月 - full name of a month. Example: January, February, etc
+ */
+public class L332 {
+    //332. 重新安排行程
+    //给你一份航线列表 tickets ，其中 tickets[i] = [fromi, toi] 表示飞机出发和降落的机场地点。请你对该行程进行重新规划排序。
+    //
+    //所有这些机票都属于一个从 JFK（肯尼迪国际机场）出发的先生，所以该行程必须从 JFK 开始。如果存在多种有效的行程，请你按字典排序返回最小的行程组合。
+    //
+    //例如，行程 ["JFK", "LGA"] 与 ["JFK", "LGB"] 相比就更小，排序更靠前。
+    //假定所有机票至少存在一种合理的行程。且所有的机票 必须都用一次 且 只能用一次。
+    //
+    //
+    //
+    //示例 1：
+    //
+    //
+    //输入：tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]
+    //输出：["JFK","MUC","LHR","SFO","SJC"]
+    //示例 2：
+    //
+    //
+    //输入：tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+    //输出：["JFK","ATL","JFK","SFO","ATL","SFO"]
+    //解释：另一种有效的行程是 ["JFK","SFO","ATL","JFK","ATL","SFO"] ，但是它字典排序更大更靠后。
+    public List<String> findItinerary(List<List<String>> tickets) {
+
+    }
+}
---
+++ src/BackStracking/L47_II.java
@@ -0,0 +1,69 @@
+package BackStracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Etherstrings
+ * @create 2022-05-23 16:32 BackStracking - the name of the target package where the new class or interface will be created.  Algorithm - the name of the current project.  null.java - the name of the PHP file that will be created.  L47_II - the name of the new file which you specify in the New File dialog box during the file creation.  ps - the login name of the current user.  2022/5/23 - the current system date.  16:32 - the current system time.  2022 - the current year.  05 - the current month.  23 - the current day of the month.  16 - the current hour.  32 - the current minute.  IntelliJ IDEA - the name of the IDE in which the file will be created.  5月 - the first 3 letters of the month name. Example: Jan, Feb, etc.  五月 - full name of a month. Example: January, February, etc
+ */
+public class L47_II {
+    //47. 全排列 II
+    //给定一个可包含重复数字的序列 nums ，按任意顺序 返回所有不重复的全排列。
+    //
+    //
+    //
+    //示例 1：
+    //
+    //输入：nums = [1,1,2]
+    //输出：
+    //[[1,1,2],
+    // [1,2,1],
+    // [2,1,1]]
+    //示例 2：
+    //
+    //输入：nums = [1,2,3]
+    //输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+    //
+    //
+    //提示：
+    //
+    //1 <= nums.length <= 8
+    //-10 <= nums[i] <= 10
+    List<List<Integer>> Result=new ArrayList<>();
+    LinkedList<Integer> Path=new LinkedList<>();
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        boolean[] Used=new boolean[nums.length];
+        BackTracking(nums,Used);
+        return Result;
+    }
+
+    private void BackTracking(int[] nums,boolean[] Used){
+        if(Path.size()== nums.length){
+            Result.add(new ArrayList<>(Path));
+            return;
+        }
+
+        for(int i=0;i< nums.length;i++){
+            //判断 当前
+            //1 2 2
+            //是不是取到了第二个2
+            //如果
+            if(i>0&&nums[i]==nums[i-1]&&Used[i-1]==false){
+                continue;
+            }
+
+            //判断当前的是不是用过了？
+            if(Used[i]==false){
+                Used[i]=true;
+                Path.add(nums[i]);
+                BackTracking(nums, Used);
+                Used[i]=false;
+                Path.removeLast();
+            }
+
+        }
+    }
+}
