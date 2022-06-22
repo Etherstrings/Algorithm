78a694a434d3a61daf3a56d8d21da74bf24e5cc8
Ether <wuboyu5659@outlook.com>
2022-06-23 00:22:54 +0800

    06.19

---
+++ src/L350.java
@@ -0,0 +1,41 @@
+/**
+ * @author Etherstrings
+ * @create 2022-06-20 20:33 PACKAGE_NAME - the name of the target package where the new class or interface will be created.  Algorithm - the name of the current project.  null.java - the name of the PHP file that will be created.  L350 - the name of the new file which you specify in the New File dialog box during the file creation.  ps - the login name of the current user.  2022/6/20 - the current system date.  20:33 - the current system time.  2022 - the current year.  06 - the current month.  20 - the current day of the month.  20 - the current hour.  33 - the current minute.  IntelliJ IDEA - the name of the IDE in which the file will be created.  6月 - the first 3 letters of the month name. Example: Jan, Feb, etc.  六月 - full name of a month. Example: January, February, etc
+ */
+public class L350 {
+    //350. 两个数组的交集 II
+    //给你两个整数数组 nums1 和 nums2 ，请你以数组形式返回两数组的交集。返回结果中每个元素出现的次数，应与元素在两个数组中都出现的次数一致（如果出现次数不一致，则考虑取较小值）。可以不考虑输出结果的顺序。
+    //
+    //
+    //
+    //示例 1：
+    //
+    //输入：nums1 = [1,2,2,1], nums2 = [2,2]
+    //输出：[2,2]
+    //示例 2:
+    //
+    //输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+    //输出：[4,9]
+    //
+    //
+    //提示：
+    //
+    //1 <= nums1.length, nums2.length <= 1000
+    //0 <= nums1[i], nums2[i] <= 1000
+    //
+    //
+    //进阶：
+    //
+    //如果给定的数组已经排好序呢？你将如何优化你的算法？
+    //如果 nums1 的大小比 nums2 小，哪种方法更优？
+    //如果 nums2 的元素存储在磁盘上，内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if(nums1.length<nums2.length){
+            //在num2中找num1
+        }else if(nums2.length<nums1.length){
+            //在num1中找num2
+        }else {
+
+        }
+    }
+}
