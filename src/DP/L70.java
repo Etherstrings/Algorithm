e80eb3935f09f2d8405a548a3595068618fbe1ef
Ether <wuboyu5659@outlook.com>
2022-07-27 21:00:11 +0800

    07.27

--- src/DP/L70.java
+++ src/DP/L70.java
@@ -48,7 +48,7 @@ public class L70 {
         dp[1]=1;
         dp[2]=2;
         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            dp[i]=dp[i-1]+dp[i-2]%1000000007;
         }
         return dp[n];
     }
