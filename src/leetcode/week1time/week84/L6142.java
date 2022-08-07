bdd7083524218c4d1621b4b633d2dab587d41a77
Ether <wuboyu5659@outlook.com>
2022-08-07 23:53:20 +0800

    08.07

---
+++ src/leetcode/week1time/week84/L6142.java
@@ -0,0 +1,63 @@
+package leetcode.week1time.week84;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Etherstrings
+ * @create 2022-08-07 11:47 leetcode.week1time.week84 - the name of the target package where the new class or interface will be created.  Algorithm - the name of the current project.  null.java - the name of the PHP file that will be created.  L6142 - the name of the new file which you specify in the New File dialog box during the file creation.  ps - the login name of the current user.  2022/8/7 - the current system date.  11:47 - the current system time.  2022 - the current year.  08 - the current month.  07 - the current day of the month.  11 - the current hour.  47 - the current minute.  IntelliJ IDEA - the name of the IDE in which the file will be created.  8月 - the first 3 letters of the month name. Example: Jan, Feb, etc.  八月 - full name of a month. Example: January, February, etc
+ */
+public class L6142 {
+    //6142. 统计坏数对的数目
+    //给你一个下标从 0 开始的整数数组 nums 。如果 i < j 且 j - i != nums[j] - nums[i] ，那么我们称 (i, j) 是一个 坏数对 。
+    //
+    //请你返回 nums 中 坏数对 的总数目
+    public long countBadPairs(int[] nums) {
+
+        //动态规划呢？
+        //当前位置的最大坏数对
+        long[] dp=new long[nums.length];
+
+        dp[0]=0;
+
+        //递推公式
+        //那就是当前这个是不是坏数队
+
+        //dp[i]+=0-i-1 所以的可能性
+        for(int i=1;i<nums.length;i++){
+            dp[i]=dp[i-1];
+            for(int j=0;j<i;j++){
+                if(isbd(nums,j,i)){
+                    dp[i]+=1;
+                }
+            }
+        }
+        return dp[nums.length-1];
+
+    }
+
+    boolean isbd(int[] nums,int j,int i){
+        //如果 i < j 且 j - i != nums[j] - nums[i] ，那么我们称 (i, j) 是一个 坏数对 。
+        if(i-j!=nums[i]-nums[j]){
+            return true;
+        }
+        return false;
+    }
+
+    public long countBadPairs1(int[] nums){
+        long n=nums.length;
+
+        long cnt=(n*(n-1)/2);
+
+        Map<Integer,Integer> map=new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            int d=nums[i]-i;
+            int t=map.getOrDefault(d,0);
+
+            cnt-=t;
+            map.put(d,t+1);
+        }
+        return cnt;
+    }
+}
---
+++ src/leetcode/week305/L6139.java
@@ -0,0 +1,58 @@
+package leetcode.week305;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Etherstrings
+ * @create 2022-08-07 15:27 leetcode.week305 - the name of the target package where the new class or interface will be created.  Algorithm - the name of the current project.  null.java - the name of the PHP file that will be created.  L6139 - the name of the new file which you specify in the New File dialog box during the file creation.  ps - the login name of the current user.  2022/8/7 - the current system date.  15:27 - the current system time.  2022 - the current year.  08 - the current month.  07 - the current day of the month.  15 - the current hour.  27 - the current minute.  IntelliJ IDEA - the name of the IDE in which the file will be created.  8月 - the first 3 letters of the month name. Example: Jan, Feb, etc.  八月 - full name of a month. Example: January, February, etc
+ */
+public class L6139 {
+    //6139. 受限条件下可到达节点的数目 显示英文描述
+    //通过的用户数3749
+    //尝试过的用户数5071
+    //用户总通过次数3851
+    //用户总提交次数10499
+    //题目难度Medium
+    //现有一棵由 n 个节点组成的无向树，节点编号从 0 到 n - 1 ，共有 n - 1 条边。
+    //
+    //给你一个二维整数数组 edges ，长度为 n - 1 ，其中 edges[i] = [ai, bi] 表示树中节点 ai 和 bi 之间存在一条边。另给你一个整数数组 restricted 表示 受限 节点。
+    //
+    //在不访问受限节点的前提下，返回你可以从节点 0 到达的 最多 节点数目。
+    //
+    //注意，节点 0 不 会标记为受限节点。
+    //
+    //
+    //
+    //示例 1：
+    //
+    //
+    //输入：n = 7, edges = [[0,1],[1,2],[3,1],[4,0],[0,5],[5,6]], restricted = [4,5]
+    //输出：4
+    //解释：上图所示正是这棵树。
+    //在不访问受限节点的前提下，只有节点 [0,1,2,3] 可以从节点 0 到达。
+    //示例 2：
+    //
+    //
+    //输入：n = 7, edges = [[0,1],[0,2],[0,5],[0,4],[3,2],[6,5]], restricted = [4,2,1]
+    //输出：3
+    //解释：上图所示正是这棵树。
+    //在不访问受限节点的前提下，只有节点 [0,5,6] 可以从节点 0 到达。
+    public int reachableNodes(int n, int[][] edges, int[] restricted) {
+        //存节点0可以到的所有节点
+        Set<Integer> Judge0=new HashSet<>();
+        for(int[] a:edges){
+            if(a[0]!=0&&a[1]!=0){
+                continue;
+            }
+            if(a[0]==0){
+                Judge0.add(a[1]);
+            }else if(a[1]==0){
+                Judge0.add(a[0]);
+            }
+        }
+
+        return -1;
+
+    }
+}
